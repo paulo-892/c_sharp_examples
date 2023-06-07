69e3f4f00547f9ce8583e1ec62356e7fe6973ebc
paulo-892 <paulo.ss.frazao@gmail.com>
2023-06-07 12:16:36 -0400

    more examples

--- exercism/csharp/darts/Darts.cs
+++ exercism/csharp/darts/Darts.cs
@@ -2,8 +2,15 @@ using System;
 
 public static class Darts
 {
+    private static double DistanceToOrigin(double x, double y) => Math.Sqrt(x * x + y * y);
+
     public static int Score(double x, double y)
     {
-       throw new NotImplementedException("You need to implement this function");
+        double dist = DistanceToOrigin(x, y);
+
+        if (dist > 10) return 0;
+        else if (dist > 5) return 1;
+        else if (dist > 1) return 5;
+        else return 10;
     }
 }
