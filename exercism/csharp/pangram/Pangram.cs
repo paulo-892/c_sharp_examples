c30ed589ed44b0435537188bb5af717a8f0806d8
paulo-892 <paulo.ss.frazao@gmail.com>
2023-06-09 15:12:49 -0400

    more examples

---
+++ exercism/csharp/pangram/Pangram.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        Dictionary<char, int> charsToCts = new Dictionary<char, int>
+        {
+            { 'a', 0 },
+            { 'b', 0 },
+            { 'c', 0 },
+            { 'd', 0 },
+            { 'e', 0 },
+            { 'f', 0 },
+            { 'g', 0 },
+            { 'h', 0 },
+            { 'i', 0 },
+            { 'j', 0 },
+            { 'k', 0 },
+            { 'l', 0 },
+            { 'm', 0 },
+            { 'n', 0 },
+            { 'o', 0 },
+            { 'p', 0 },
+            { 'q', 0 },
+            { 'r', 0 },
+            { 's', 0 },
+            { 't', 0 },
+            { 'u', 0 },
+            { 'v', 0 },
+            { 'w', 0 },
+            { 'x', 0 },
+            { 'y', 0 },
+            { 'z', 0 }
+        };
+
+        foreach (var c in input.Where(c => char.IsLetter(char.ToLower(c)))) charsToCts[char.ToLower(c)]++;
+        
+        return charsToCts.All(kvp => kvp.Value > 0);
+    }
+}
