cd14faeb712c3115adedc767a27ded4019fa95d9
paulo-892 <paulo.ss.frazao@gmail.com>
2023-06-11 23:37:47 -0400

    more examples

---
+++ exercism/csharp/instruments-of-texas/InstrumentsOfTexas.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Runtime.CompilerServices;
+
+public class CalculationException : Exception
+{
+    public CalculationException(int operand1, int operand2, string message, Exception inner) : base(message, inner)
+    {
+        Operand1 = operand1;
+        Operand2 = operand2;
+    }
+    
+
+    public int Operand1 { get; }
+    public int Operand2 { get; }
+}
+
+public class CalculatorTestHarness
+{
+    private Calculator calculator;
+
+    public CalculatorTestHarness(Calculator calculator)
+    {
+        this.calculator = calculator;
+    }
+
+    public string TestMultiplication(int x, int y)
+    {
+        try
+        {
+            Multiply(x, y);
+            return "Multiply succeeded";
+        }
+        catch (CalculationException e) when (x < 0 && y < 0)
+        {
+            return $"Multiply failed for negative operands. {e.InnerException.Message}";
+        }
+        catch (CalculationException e) when (x > 0 || y > 0)
+        {
+            return $"Multiply failed for mixed or positive operands. {e.InnerException.Message}";
+        }
+    }
+
+    public void Multiply(int x, int y)
+    {
+        try
+        {
+            calculator.Multiply(x, y);
+        }
+        catch (OverflowException e)
+        {
+            throw new CalculationException(x, y, "Test message", e);
+        }
+    }
+}
+
+
+// Please do not modify the code below.
+// If there is an overflow in the multiplication operation
+// then a System.OverflowException is thrown.
+public class Calculator
+{
+    public int Multiply(int x, int y)
+    {
+        checked
+        {
+            return x * y;
+        }
+    }
+}
